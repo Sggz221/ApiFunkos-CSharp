67c2c4043dbca1a7945960abea6bde0e129942a9
Sggz221 <sggz221@gmail.com>
2026-01-14 12:13:03 +0100

    validaciones en dto

--- cSharpApiFunko/Models/Dto/FunkoRequestDto.cs
+++ cSharpApiFunko/Models/Dto/FunkoRequestDto.cs
@@ -1,4 +1,7 @@
-﻿namespace cSharpApiFunko.Models.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace cSharpApiFunko.Models.Dto;
 
 public record FunkoRequestDto(
     string Nombre,
@@ -6,7 +9,11 @@ public record FunkoRequestDto(
     double Precio
     )
 {
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener entre 2 y 100 caracteres")]
     public string Nombre { get; set; } = Nombre;
+    [Required(ErrorMessage = "El campo categoria es obligatorio")]
     public string Categoria { get; set; } = Categoria;
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser positivo")]
     public double Precio { get; set; } = Precio;
 }
